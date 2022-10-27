b04cee4dccdce003dd2fc15c7d47fff728f50296
chobae <tmsprqo@gmail.com>
2022-10-27 22:06:24 +0900

    RBt complete

--- src/rbtree.h
+++ src/rbtree.h
@@ -27,6 +27,14 @@ node_t *rbtree_min(const rbtree *);
 node_t *rbtree_max(const rbtree *);
 int rbtree_erase(rbtree *, node_t *);
 
+void postorder_del(rbtree *, node_t *);
+void left_rotate(rbtree *, node_t *);
+void right_rotate(rbtree *, node_t *);
+void insert_fixup(rbtree *, node_t *);
+node_t *find_min(const rbtree *, node_t *);
+void transplant(rbtree *, node_t *, node_t *);
+void del_fixup(rbtree *, node_t *);
+int inorder_search(const rbtree *, node_t *, int, key_t *, int);
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
 
 #endif  // _RBTREE_H_
